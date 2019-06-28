0b11805be201f2e423e8f12079355225bced2dea
Paul Fenney <paul.fenney@pennantplc.co.uk>
2019-06-28 10:17:04 +0100

    Script to clean up broken git refs.
    Includes how to grep stderr.

---
+++ bin/clean-broken-git-refs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# find any local refs which generate an error and remove them
+# This can be used to fix the following error message from git commands:
+#
+#    error: refs/remotes/origin/branchname does not point to a valid object!
+#
+
+git show-ref |& \
+    grep -Po '(?<=error: refs/remotes/origin/)[^ ]+' | \
+    xargs -i{} git push origin :refs/heads/{}
