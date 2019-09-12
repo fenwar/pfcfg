8a99bef174c925c248ac94d4949b3281ede5d6c4
Paul Fenney <paul.fenney@pennantplc.co.uk>
2019-09-12 10:46:02 +0100

    Make it easier to configure the clock font.

--- bin/monitor-time.sh
+++ bin/monitor-time.sh
@@ -4,6 +4,7 @@
 # This is intended to run in a tmux monitor window.
 
 DATE_FORMAT="%H:%M"
+FIGLET_FONT="mono12"
 
 watch -p -n1 -t -c \
-    'echo "$(tput setaf 1)$(date +'${DATE_FORMAT}' | figlet -t -c -f smmono12)"'
+    'echo "$(tput setaf 1)$(date +'${DATE_FORMAT}' | figlet -t -c -f '${FIGLET_FONT}')"'
