e3684cdcfc47b21db05a33d733c68b4c33e519a7
Paul Fenney <paul.fenney@pennantplc.co.uk>
2019-09-12 10:34:39 +0100

    Added note about the purpose of the netifstate script.

--- bin/netifstate.sh
+++ bin/netifstate.sh
@@ -2,6 +2,7 @@
 
 # Monitor the systemd network status file for changes and display the
 # current IP address when it updates.
+# This is intended to run in a tmux monitor window.
 
 NETIFSTATE=/run/systemd/netif/state
 
