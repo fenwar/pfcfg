59440fea12f16a56cfb1a30fadbc5f94caf3f973
Paul Fenney <paul.fenney@pennantplc.co.uk>
2019-09-23 10:31:21 +0100

    Display the date below the clock

--- bin/monitor-time.sh
+++ bin/monitor-time.sh
@@ -3,8 +3,9 @@
 # Display a terminal clock using figlet
 # This is intended to run in a tmux monitor window.
 
-DATE_FORMAT="%H:%M"
+TIME_FORMAT="%H:%M"
+DATE_FORMAT="%d-%m-%Y   "
 FIGLET_FONT="mono12"
 
 watch -p -n1 -t -c \
-    'echo "$(tput setaf 1)$(date +'${DATE_FORMAT}' | figlet -t -c -f '${FIGLET_FONT}')"'
+    'echo "$(tput setaf 1)$(date +'${TIME_FORMAT}' | figlet -t -c -f '${FIGLET_FONT}')$(tput op)" ; echo "$(date +'${DATE_FORMAT}' | figlet -t -c -f wideterm -w72)"'
