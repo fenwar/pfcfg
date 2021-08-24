9e171918018c8c28ba22d380b16df11a947f20ef
Paul Fenney <paul.fenney@pennantplc.co.uk>
2021-08-24 10:08:19 +0100

    Updated window auto-refresh script and added my markdown preview entr script.

--- bin/refresh.sh
+++ bin/refresh.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 # Refresh the browser window with supplied title.
 
+WINDOW_TITLE=$1
+KEYPRESS="Control_L+F5"
+
+echo "Sending ${KEYPRESS} to browser window named ${WINDOW_TITLE}"
+
 # Find the window to be refreshed
-TARGET_WINDOW=$(xdotool search --name "$1")
+TARGET_WINDOW=$(xdotool search --onlyvisible --name "${WINDOW_TITLE}")
 
 # Store the previously active window so we can switch back to it:
 PREVIOUS_WINDOW=$(xdotool getactivewindow)
 
 # Focus the browser window and refresh it:
-xdotool windowactivate $TARGET_WINDOW key --window $TARGET_WINDOW F5
+xdotool windowactivate --sync $TARGET_WINDOW key --window $TARGET_WINDOW $KEYPRESS
 
 # Switch back to the previously active window:
 xdotool windowactivate $PREVIOUS_WINDOW
