552a97e5c7432999ebed2e18ce0499366e98ef6b
Paul Fenney <paul.fenney@pennantplc.com>
2024-03-23 14:23:00 +0000

    Install script I should have made years ago.

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Install basic symlinks only
+# Assume git is already installed
+# Other packages:
+#  tmux
+#  ack dwdiff diffutils patchutils exuberant-ctags
+#  vim-nox vim-command-t vim-youcompleteme
+#  tree
+
+REPO_PATH=$(dirname $0)
+
+ln -sf ${REPO_PATH}/.ackrc ${HOME}/.ackrc
+ln -sf ${REPO_PATH}/.bash_aliases ${HOME}/.bash_aliases
+ln -sf ${REPO_PATH}/.dwdiffrc ${HOME}/.dwdiffrc
+ln -sf ${REPO_PATH}/.tmux.conf ${HOME}/.tmux.conf
+ln -sf ${REPO_PATH}/.vimrc ${HOME}/.vimrc
+
+git config --global push.default current
+git config --global core.excludesFile ${REPO_PATH}/.pf-gitignore
