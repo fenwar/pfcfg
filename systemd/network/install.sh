d319e95fa6ed5a82e211868a7436ae1a7ede1b93
Paul Fenney <fenney@gmail.com>
2025-11-13 23:19:05 +0000

    Don't forget to start resolved as well

--- systemd/network/install.sh
+++ systemd/network/install.sh
@@ -5,3 +5,4 @@ sed -e "s/en[*]/${ETHER}/g" < ether.network \
 #sudo systemctl enable systemd-networkd
 #sudo apt install systemd-resolved
 #sudo ip link set ${ETHER} down ; sudo apt purge ifupdown -y ; sudo systemctl restart systemd-networkd.service
+#sudo systemctl restart systemd-resolved.service
