6e8c28d67afc662e8f6c143725cdd5e5c033da38
Paul Fenney <fenney@gmail.com>
2025-11-15 20:55:38 +0000

    Change order of purge ifupdown and set link down, give ssh connection a chance to stay up.

--- systemd/network/install.sh
+++ systemd/network/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-ETHER=enp4s0f0
+ETHER=enp3s0
 sed -e "s/en[*]/${ETHER}/g" < ether.network \
     | sudo tee /etc/systemd/network/${ETHER}.network
 #sudo systemctl enable systemd-networkd
 #sudo apt install systemd-resolved
-#sudo ip link set ${ETHER} down ; sudo apt purge ifupdown -y ; sudo systemctl restart systemd-networkd.service
+#sudo apt purge ifupdown -y ; sudo ip link set ${ETHER} down ; sudo systemctl restart systemd-networkd.service
 #sudo systemctl restart systemd-resolved.service
